ff23535c841f8a8f71040fbdb4d1caf86a57684c
RafiHaqul <94788343+RafiHaqul@users.noreply.github.com>
2024-05-15 03:37:17 +0700

    tailwind

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -13,7 +13,7 @@
         <div class="container mx-auto flex justify-between items-center">
             <a class="text-white text-xl font-bold" href="#">
                 <img src="{{ asset('image/logo.png') }}" alt="Logo" class="h-8 inline-block">
-                NAMA JASA
+                AniArt
             </a>
             <div class="space-x-4">
                 <a class="text-gray-300 hover:text-white" href="#">Beranda</a>
